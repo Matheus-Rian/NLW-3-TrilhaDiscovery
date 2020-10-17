c7cbd6efc983a2709a5c4d49cb2c6a5b262b6af3
Matheus Rian <souzamatheusrian@gmail.com>
2020-10-16 23:28:15 -0300

    Evoluindo o projeto

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -40,6 +40,43 @@ function addPhotoField() {
     const fieldsContainer = document.querySelectorAll('.new-upload')
     //realizar o clone da última imagem adicionada.
     const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+    //Verificar se o campo está vazio, se sim, não add ao container de images
+    const input = newFieldContainer.children[0]
+    if(input.value == ""){
+        return
+    }
+    //Limpar o campo antes de add ao container de images
+    input.value = ""
     // Adicionar o clone ao container de #iages
     container.appendChild(newFieldContainer)
+}
+
+function deleteField(event){
+    const span = event.currentTarget
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    if(fieldsContainer.length <= 1){
+    //limpar o valor do campo
+        span.parentNode.children[0].value = ""
+        return
+    }
+
+    //deletar o campo
+    span.parentNode.remove()
+}
+
+//Selecet yes or np
+function toggleSelect(event) {
+
+    //retirar a class .active dos botões
+    document.querySelectorAll('.button-select button')
+    .forEach(button => button.classList.remove('active'))
+    //Colocar a class .active nesse botão clicado
+    const button = event.currentTarget
+    button.classList.add('active')
+    //Atualizar o meu input hidden com o valor selecionado
+    const input = document.querySelector('[name="open_on_weekends"]')
+    //Verificar sim ou não
+    input.value = button.dataset.value
+
 }
